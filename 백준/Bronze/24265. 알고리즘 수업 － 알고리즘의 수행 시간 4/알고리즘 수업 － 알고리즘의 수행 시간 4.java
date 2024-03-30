1f7a37dea74f0fecc65df321aee7e7ce84404433
jinu0729 <108972941+jinu0729@users.noreply.github.com>
2024-03-30 13:33:46 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -1,15 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        br.close();
+        long n = Integer.parseInt(br.readLine()) - 1;
+        long answer = calculateSum(n);
 
-        bw.write((long) n*(n-1)/2 + "\n" + 2);
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
+        System.out.println(2);
+    }
+
+    public static long calculateSum(long n) {
+        long sum = 0;
+        for (long i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }

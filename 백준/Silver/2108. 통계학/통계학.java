c0db38e91327358b57e9cf840561401b046f5be6
jinu0729 <108972941+jinu0729@users.noreply.github.com>
2024-04-07 17:56:18 +0900

    [Silver III] Title: 통계학, Time: 592 ms, Memory: 47308 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * --------------------------------------------------------------
@@ -29,69 +27,56 @@ public class Main {
         // 게임 횟수
         int N = Integer.parseInt(br.readLine());
 
-        // 정수를 담을 arr
-        int[] arr = new int[N];
+        // 정수를 담을 배열
+        int[] arr1 = new int[N];
 
         // 산술평균을 위한 sum
         int sum = 0;
 
+        // 최빈값을 위한 배열, -4000 ~ 4000까지 값 확인
+        int[] arr2 = new int[8001];
+
         // 정수들을 담음
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+            arr1[i] = Integer.parseInt(br.readLine());
+            sum += arr1[i];
+            arr2[arr1[i] + 4000]++;
         }
 
         // 정렬
-        Arrays.sort(arr);
+        Arrays.sort(arr1);
 
         // 산술평균, 소수점 이하 첫째 자리에서 반올림
-        sb.append((int) Math.round((double) sum / arr.length) + "\n");
-        // 중간값
-        sb.append(arr[(arr.length - 1) / 2] + "\n");
+        sb.append((int) Math.round((double) sum / arr1.length) + "\n");
+        // 중앙값
+        sb.append(arr1[(arr1.length - 1) / 2] + "\n");
         // 최빈값, 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값 출력
-        sb.append(mode(arr) + "\n");
+        sb.append(mode(arr2) + "\n");
         // 범위
-        sb.append(arr[(arr.length - 1)] - arr[0]);
+        sb.append(arr1[(arr1.length - 1)] - arr1[0]);
 
         System.out.println(sb);
     }
 
-    public static int mode(int[] arr) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+    public static int mode(int[] arr2) {
+        int maxCount = 0;
 
-        // {해당 정수, count}
-        for (int i = 0; i < arr.length; i++) {
-            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
-        }
-
-        // 두번째 작은값 출력을 위해 PriorityQueue 생성
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        // arr에서 맨 처음 값 입력
-        pq.offer(arr[0]);
-        int num = arr[0];
-        int count = hashMap.get(arr[0]);
-
-        for (int i = 1; i < arr.length; i++) {
-            // count가 높을 경우
-            if (hashMap.get(arr[i]) > count) {
-		        // 성준님이 알려주심!
-                pq.clear();
-                pq.offer(arr[i]);
-                num = arr[i];
-                count = hashMap.get(arr[i]);
-            // 중복 방지를 위해 count가 같지만 정수가 같지 않을때만 offer한다
-            } else if (hashMap.get(arr[i]) == count && num != arr[i]) {
-                pq.offer(arr[i]);
+        for (int i = 0; i < 8001; i++) {
+            if (arr2[i] > maxCount) {
+                maxCount = arr2[i];
             }
         }
 
-        // 최빈값이 같을 경우 두번쨰 작은수 return
-        if (pq.size() > 1) {
-            pq.poll();
-            return pq.poll();
+        //최빈값 확인
+        ArrayList<Integer> mode = new ArrayList<>();
+        for (int i = 0; i < 8001; i++) {
+            if (arr2[i] == maxCount) {
+                mode.add(i - 4000);
+            }
         }
 
-        return pq.poll();
+        Collections.sort(mode);
+
+        return (mode.size() > 1) ? mode.get(1) : mode.get(0);
     }
 }

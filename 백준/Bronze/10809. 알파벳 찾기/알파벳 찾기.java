5ef17e9acaeaf86d6badfc0f0dce170c41cca4fd
jinu0729 <108972941+jinu0729@users.noreply.github.com>
2024-07-25 02:33:47 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            arr[i] = -1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(arr[c - 'a'] == -1) {
+                arr[c - 'a'] = i;
+            }
+        }
+
+        for (int i : arr) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+    }
+}
